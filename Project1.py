edf4f21e7b1adbe29a1be94e47aec50235073151
moinmj <moinmj7@gmail.com>
2025-08-08 23:56:55 +0530

    My first Commit

---
+++ Project1.py
@@ -0,0 +1,152 @@
+from fastapi import FastAPI , Path , Query ,HTTPException, status
+from pydantic import BaseModel, Field
+from typing import Optional
+import pymongo
+from datetime import date, datetime
+# from motor.motor_asyncio import AsyncIOMotorClient
+print("Welcome to pmongo")
+client=pymongo.MongoClient("mongodb://localhost:27017/") #this here is connection to by database
+print(client)
+db=client["Learning"] #making database name,forming a database
+collection=db["expense"]
+collection1=db["categories"]
+
+app=FastAPI()
+
+class Expense(BaseModel):
+    amount:int 
+    category:str
+    date:str=Field(...,description="Provide in YYYY-MM-DD format",example="YYYY-MM-DD") #These here are fields especially given description to date field to provide format of date
+    # start_date:Optional[date]=Query(None,description="Provide in YYYY-MM-DD format")
+    # end_date:str=Field(...,description="Provide in YYYY-MM-DD format",example="YYYY-MM-DD")
+    description:str
+
+# inventory = {}
+
+
+
+
+# def break_date(date): #defined a function for breaking time
+
+#     date = date #format YYYY-MM-DD
+#     date_obj = datetime.strptime(date, "%m") #This is a function or method that is used to break date in year,Month and day imported from datetime library
+#     # date_obj = datetime.strptime(date, "%Y-%m-%d") #This is a function or method that is used to break date in year,Month and day imported from datetime library
+#     # month = date_obj.month #seperated month and stored in variable month
+#     return date_obj
+
+
+
+
+@app.post("/expense/{item_id}")
+def add_expense(item_id: int, expense: Expense):
+    if collection.find_one({"item_id": item_id}):
+        raise HTTPException(status_code=400, detail="Item ID already exists")
+    
+    # month = break_date(expense.date)
+    
+    expense_data = {
+        "item_id": item_id,
+        "amount": expense.amount,
+        "category": expense.category.lower(),
+        "date":expense.date,
+        # "month": month,
+        # "start_date": expense.start_date,
+        # "end_date": expense.end_date,
+        "description": expense.description
+    }
+    collection.insert_one(expense_data)
+    return {"message": "Expenses added successfully"}
+@app.post ("/categories/")
+
+@app.get("/allexpense/{item_id}")
+def get_expense(item_id: int = Path(description="The ID of the expense to retrieve")):
+    expense = collection.find_one({"item_id": item_id})
+    if not expense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found")
+    if "_id" in expense:
+        expense["_id"] = str(expense["_id"])
+    return expense
+# @app.get("/expensebydate/{item_id}")
+# def get_expense(*,item_id:int,start_date: date = Query(...), end_date: date = Query(...)):
+# # def get_expense(*,item_id: int ,start_date:str,end_date:str,test:int):
+#     expense = collection.find_one({"item_id": item_id})
+#     if not expense:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found")
+#     if "_id" in expense:
+#         expense["_id"] = str(expense["_id"])
+#     return expense
+@app.get("/expensebydate/", summary="Get expenses by item ID and date range")
+def get_expense(
+    *,start_date: date = Query(description="Start date in YYYY-MM-DD format", example="2025-01-01"),  # item_id: int = Path(description="The ID of the expense to retrieve"),
+    end_date: date = Query(description="End date in YYYY-MM-DD format", example="2025-01-31")
+):
+    query={
+        "date": {
+            "$gte": start_date.isoformat(),
+            "$lte":end_date.isoformat()
+
+        }
+    }
+    expenses=list(collection.find({query}))
+    if not expenses:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No expenses found for this categpry")
+    for expense in expenses:
+        if "_id" in expense:
+            expense["_id"] = str(expense["_id"])
+    return expenses
+
+@app.get("/get-item-by-category/")
+def get_item_by_category(*,category:str= Query(...,description="You can get any category")):
+    expenses=list(collection.find({"category":category}))
+    if not expenses:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No expenses found for this categpry")
+    for expense in expenses:
+        if "_id" in expense:
+            expense["_id"] = str(expense["_id"])
+    return expenses
+
+@app.get("/expensebymonth/")
+def get_expenses_by_month(month: int = Query(..., description="Month as a number (1-12)")):
+    # Fetch all records
+      # Use empty filter to get all documents
+    expenses = list(collection.find({ "month":month}))
+    # Filter based on the month extracted from string-formatted date
+    # filtered_expenses = []
+    for expense in expenses:
+         if "_id" in expense:
+            expense["_id"] = str(expense["_id"])
+    return expenses
+        # try:
+        #     # Convert the date string to datetime object
+        #     if "date" in expense:
+        #         record_date = datetime.strptime(expense["date"], "%Y-%m-%d")
+        #         if record_date.month == month:
+        #             # Convert ObjectId to string before adding to results
+        #             expense["_id"] = str(expense["_id"])
+        #             filtered_expenses.append(expense)
+        # except (ValueError, KeyError):
+        #     continue  # skip if date is malformed or missing
+
+    # if not filtered_expenses:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND, 
+    #         detail=f"No expenses found for month {month}"
+    #     )
+
+    # return filtered_expenses
+@app.get("/top-categories")
+def get_top_categories():
+    expense = [{"$group": {"_id": "$category", "total_amount": {"$sum": "$amount"}}},
+        {"$sort": {"total_amount": -1}},
+        {"$limit": 3}
+    ]
+    # expense = [
+    #     {"$group": {"_id": "$category", "total_amount": {"$sum": "$amount"}}},
+    #     {"$sort": {"total_amount": -1}},
+    #     {"$limit": 3}
+    # ]
+    expenses = list(collection.aggregate(expense))
+    return expenses
+    # result = list(collection.aggregate(expense))
+    # return [{"category": r["_id"], "total_amount": r["total_amount"]} for r in result]
+
---
+++ expense_tracker_app.py
@@ -0,0 +1,242 @@
+import streamlit as st
+import requests #Used to send HTTP requests (GET, POST) to the FastAPI backend.
+from datetime import datetime, date
+
+API_URL = "http://127.0.0.1:8000"
+
+
+st.title("Add New Expense")
+action=st.sidebar.selectbox("select",("Add","Get","View by date","view by category","view by month","top spending category"))   
+if action=="Add":
+    with st.form("expense_form"): #Forms a Form -->We use with st.form(...) to group multiple input widgets together into a single form, so that they are only processed when a specific submit button is clicked.
+            item_id = st.text_input("Item ID")
+            amount = st.number_input("Amount", min_value=0)
+            category = st.selectbox("Category",["Food","Salary","clothes"])
+            # category = st.text_input("Category")
+            date_val = st.date_input("Date")
+            description = st.text_input("Description")
+            
+            submit_button = st.form_submit_button("Add Expense") #And here I formed submit button that triggers processing of all form inputs at once.
+            
+            if submit_button and category and amount and date_val:  # Basic validation until these inputs are inserted it won't submit
+                expense_data = {
+                    "amount": int(amount),      #Prepares a JSON-like Python dict to send to the backend as HTTP doesn't take python object
+                    "category": str(category).lower(),
+                    "date": date_val.strftime("%Y-%m-%d"),
+                    "description": str(description)
+                }
+                
+                
+                response = requests.post( f"{API_URL}/expense/{item_id}",json=expense_data)  #request.post is a function that sends a post request to the backend route, and turns data into json   
+                    
+                    
+                if response.status_code == 200: #status_code is an HTML code if 200 it means success, 404 is error 201 is created
+                        st.success("Expense added successfully!")
+                        st.json(response.json())
+                else:
+                        st.error(f"Error: {response.json()['detail']}") #returns error message that is item not found
+
+
+elif action == "Get":
+    st.header("View Expense by ID")
+    item_id = st.number_input("Enter Item ID", min_value=1, step=1)
+    if st.button("Search"):
+            try:
+                response = requests.get(f"{API_URL}/allexpense/{item_id}") #get api hit
+                if response.status_code == 200: #if success
+                    expense = response.json() #will send data to expense
+                    st.write("### Expense Details")
+                    st.write(f"**Amount:** ${expense['amount']}")
+                    st.write(f"**Category:** {expense['category']}") #all the data that will be displayed
+                    st.write(f"**Date:** {expense['date']}")
+                    st.write(f"**Description:** {expense['description']}")
+                else:
+                    st.error(f"Error: {response.json()['detail']}")
+            except requests.exceptions.RequestException as e:
+                st.error(f"Failed to connect to the server: {str(e)}")
+
+elif action=="View by date":
+    st.header("View Expenses by Date Range")
+    col1, col2 = st.columns(2) #This widget forms two cloumns
+    with col1:
+            start_date = st.date_input("Start Date") #in col1 takes date input
+    with col2:
+            end_date = st.date_input("End Date") #in col2 takes date input as well
+        
+    if st.button("Search by Date"):
+            # try:
+                response = requests.get(
+                    f"{API_URL}/expensebydate/",  #as this get method takes 2 query method so we provide it with 2 parameters
+                    params={"start_date": start_date.strftime("%Y-%m-%d"),"end_date": end_date.strftime("%Y-%m-%d")}
+                )
+                if response.status_code == 200:
+                    expenses = response.json()
+                    st.write(expenses)
+                else:
+                    st.error(f"Error: {response.json()['detail']}")
+            # except requests.exceptions.RequestException as e:
+            #     st.error(f"Failed to connect to the server: {str(e)}")
+
+elif action=="view by category":
+    st.header("View Expenses by Category")
+    category = st.text_input("Enter Category")
+    if st.button("Search by Category"):
+                response = requests.get( f"{API_URL}/get-item-by-category/", params={"category": category.lower()}
+                )  #this method here takes only 1 query parameter category
+                if response.status_code == 200:
+                    expenses = response.json()
+                    st.write(expenses)
+                else:
+                    st.error(f"Error: {response.json()['detail']}")
+
+elif action=="view by month":
+    st.header("View Expenses by Month")
+    month = st.number_input("Enter Month (1-12)", min_value=1, max_value=12, step=1)
+    if st.button("Search by Month"): 
+                response = requests.get(f"{API_URL}/expensebymonth/",params={"month": month}
+                ) #This get api method takes one query parameter that is month ,date i already broke down to year,month and day
+                if response.status_code == 200:
+                    expenses = response.json()
+                    st.write(expenses)
+                    
+                else:
+                    st.error(f"Error: {response.json()['detail']}")
+
+elif action=="top spending category":
+    st.header("Top Spending Categories")
+    if st.button("Show Top Categories"):
+                response = requests.get(f"{API_URL}/top-categories")
+                if response.status_code == 200:
+                    categories = response.json()
+                    st.write("### Top 3 Spending Categories")
+                    for idx, category in enumerate(categories, 1):
+                        st.write(f"{idx}. **{category['_id']}**: ${category['total_amount']}")
+                else:
+                    st.error(f"Error: {response.json()['detail']}")
+
+# def add_expense(): #This function is called from API
+#     st.title("Add New Expense")
+    
+#     with st.form("expense_form"): #Forms a Form -->We use with st.form(...) to group multiple input widgets together into a single form, so that they are only processed when a specific submit button is clicked.
+#         item_id = st.text_input("Item ID")
+#         amount = st.number_input("Amount", min_value=0)
+#         category = st.text_input("Category")
+#         date_val = st.date_input("Date")
+#         description = st.text_input("Description")
+        
+#         submit_button = st.form_submit_button("Add Expense") #And here I formed submit button that triggers processing of all form inputs at once.
+        
+#         if submit_button and category and amount and date_val:  # Basic validation until these inputs are inserted it won't submit
+#             expense_data = {
+#                 "amount": int(amount),      #Prepares a JSON-like Python dict to send to the backend as HTTP doesn't take python object
+#                 "category": str(category).lower(),
+#                 "date": date_val.strftime("%Y-%m-%d"),
+#                 "description": str(description)
+#             }
+            
+            
+#             response = requests.post( f"{API_URL}/expense/{item_id}",json=expense_data)  #request.post is a function that sends a post request to the backend route, and turns data into json   
+                 
+                
+#             if response.status_code == 200: #status_code is an HTML code if 200 it means success, 404 is error 201 is created
+#                     st.success("Expense added successfully!")
+#                     st.json(response.json())
+#             else:
+#                     st.error(f"Error: {response.json()['detail']}") #returns error message that is item not found
+
+
+# def view_expense_by_id():
+#     st.header("View Expense by ID")
+#     item_id = st.number_input("Enter Item ID", min_value=1, step=1)
+#     if st.button("Search"):
+#         try:
+#             response = requests.get(f"{API_URL}/allexpense/{item_id}") #get api hit
+#             if response.status_code == 200: #if success
+#                 expense = response.json() #will send data to expense
+#                 st.write("### Expense Details")
+#                 st.write(f"**Amount:** ${expense['amount']}")
+#                 st.write(f"**Category:** {expense['category']}") #all the data that will be displayed
+#                 st.write(f"**Date:** {expense['date']}")
+#                 st.write(f"**Description:** {expense['description']}")
+#             else:
+#                 st.error(f"Error: {response.json()['detail']}")
+#         except requests.exceptions.RequestException as e:
+#             st.error(f"Failed to connect to the server: {str(e)}")
+
+# def view_expenses_by_date():
+#     st.header("View Expenses by Date Range")
+#     col1, col2 = st.columns(2) #This widget forms two cloumns
+#     with col1:
+#         start_date = st.date_input("Start Date") #in col1 takes date input
+#     with col2:
+#         end_date = st.date_input("End Date") #in col2 takes date input as well
+    
+#     if st.button("Search by Date"):
+#         # try:
+#             response = requests.get(
+#                 f"{API_URL}/expensebydate/",  #as this get method takes 2 query method so we provide it with 2 parameters
+#                 params={"start_date": start_date.strftime("%Y-%m-%d"),"end_date": end_date.strftime("%Y-%m-%d")}
+#             )
+#             if response.status_code == 200:
+#                 expenses = response.json()
+#                 display_expenses(expenses)
+#             else:
+#                 st.error(f"Error: {response.json()['detail']}")
+#         # except requests.exceptions.RequestException as e:
+#         #     st.error(f"Failed to connect to the server: {str(e)}")
+
+# def view_expenses_by_category():
+#     st.header("View Expenses by Category")
+#     category = st.text_input("Enter Category")
+#     if st.button("Search by Category"):
+#             response = requests.get( f"{API_URL}/get-item-by-category/", params={"category": category.lower()}
+#             )  #this method here takes only 1 query parameter category
+#             if response.status_code == 200:
+#                 expenses = response.json()
+#                 display_expenses(expenses)
+#             else:
+#                 st.error(f"Error: {response.json()['detail']}")
+
+# def view_expenses_by_month():
+#     st.header("View Expenses by Month")
+#     month = st.number_input("Enter Month (1-12)", min_value=1, max_value=12, step=1)
+#     if st.button("Search by Month"): 
+#             response = requests.get(f"{API_URL}/expensebymonth/",params={"month": month}
+#             ) #This get api method takes one query parameter that is month ,date i already broke down to year,month and day
+#             if response.status_code == 200:
+#                 expenses = response.json()
+#                 display_expenses(expenses)
+#             else:
+#                 st.error(f"Error: {response.json()['detail']}")
+
+# def view_top_categories():
+#     st.header("Top Spending Categories")
+#     if st.button("Show Top Categories"):
+#             response = requests.get(f"{API_URL}/top-categories")
+#             if response.status_code == 200:
+#                 categories = response.json()
+#                 st.write("### Top 3 Spending Categories")
+#                 for idx, category in enumerate(categories, 1):
+#                     st.write(f"{idx}. **{category['_id']}**: ${category['total_amount']}")
+#             else:
+#                 st.error(f"Error: {response.json()['detail']}")
+
+# def display_expenses(expenses): #Helper function to display expenses in a consistent format
+   
+#     if not expenses:
+#         st.warning("No expenses found!")
+#         return
+    
+#     for expense in expenses:
+#         st.write("---")
+#         st.write(f"**ID:** {expense['item_id']}")
+#         st.write(f"**Amount:** ${expense['amount']}")
+#         st.write(f"**Category:** {expense['category']}")
+#         st.write(f"**Date:** {expense['date']}")
+#         st.write(f"**Description:** {expense['description']}")
+# add_expense()
+# view_expense_by_id()
+# view_expenses_by_date()
+# view_expenses_by_category()
+# view_expenses_by_month()
+# view_top_categories()
